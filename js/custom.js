c5a62595bbcf27c96d17c63008dfa1fc1db0a2e6
Akhsaul <ikhsanmaulana@gmail.com>
2024-01-16 21:33:22 +0700

    custom js, rev.5

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,4 @@ document.getElementById("mainLayer").remove();
 function addJs(src){var scriptElement=document.createElement("script");scriptElement.src=src;document.body.appendChild(scriptElement);}
 addJs("https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js");addJs("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js");
 function addCss(src){var linkElement=document.createElement("link");linkElement.rel="stylesheet";linkElement.type="text/css";linkElement.href=src;document.head.appendChild(linkElement);}
-addCss("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css")
+addCss("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css");alert("Ikhsan Maulana, XSS detected");
