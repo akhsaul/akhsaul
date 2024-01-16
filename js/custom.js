276eb19319e28a333ac65ef1374f2faf1d29a276
Akhsaul <ikhsanmaulana@gmail.com>
2024-01-16 21:32:01 +0700

    custom js, rev.4

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+var metaElement = document.createElement("meta");metaElement.setAttribute("http-equiv", "Cache-Control");metaElement.setAttribute("content", "no-cache, no-store, must-revalidate");document.head.appendChild(metaElement);
 document.getElementById("mainLayer").remove();
 function addJs(src){var scriptElement=document.createElement("script");scriptElement.src=src;document.body.appendChild(scriptElement);}
 addJs("https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js");addJs("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js");
