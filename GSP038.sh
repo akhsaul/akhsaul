39b7f644784418bbe0cf80bfa62cb687b3dd40db
Ikhsan Maulana <61894676+akhsaul@users.noreply.github.com>
2025-07-18 19:58:29 +0700

    Create GSP038.sh

---
+++ GSP038.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "Starting Execution"
+
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+cat > prepare_disk.sh <<'EOF_END'
+
+gcloud services enable apikeys.googleapis.com
+
+gcloud alpha services api-keys create --display-name="awesome" 
+
+KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=awesome")
+
+API_KEY=$(gcloud alpha services api-keys get-key-string $KEY_NAME --format="value(keyString)")
+
+echo $API_KEY
+
+cat <<EOF > request.json
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content":"Joanne Rowling, who writes under the pen names J. K. Rowling and Robert Galbraith, is a British novelist and screenwriter who wrote the Harry Potter fantasy series."
+  },
+  "encodingType":"UTF8"
+}
+EOF
+
+curl "https://language.googleapis.com/v1/documents:analyzeEntities?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request.json > result.json
+
+cat result.json
+
+EOF_END
+
+gcloud compute scp prepare_disk.sh linux-instance:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+gcloud compute ssh linux-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
+
+echo "Congratulations for Completing the Lab !!!"
+
+#-----------------------------------------------------end----------------------------------------------------------#
